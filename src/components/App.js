b7bab9b4dabb53429472d51e1f2a459e37f38c08
Tess Lameyer <tjlameyer@gmail.com>
2020-02-10 16:04:14 -0800

    set up the Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { 
+  BrowserRouter as Router, 
+  Route 
+} from 'react-router-dom';
 import Header from './Header/Header';
 import SignUp from '../components/Auth/SignUp/SignUp';
+import Login from '../components/Auth/Login/Login';
 import Footer from './Footer/Footer.js';
 
 export default function App() {
@@ -8,7 +13,12 @@ export default function App() {
   return (
     <>
       <Header />
-      <SignUp />
+
+      <Router>
+        <Route exact path='/' component={SignUp} />
+        <Route path='/login'component={Login} />
+      </Router>
+
       <Footer />
     </>
   );
