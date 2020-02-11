8abe3a64beda95e081e7ab2166223a7923176fff
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2020-02-11 14:14:15 -0800

    function tested

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -19,30 +19,35 @@ const Details = () => {
   const getDataPoints = (dataSessionId) => {
     fetch(`/api/v1/pi-data-points/${dataSessionId}`)
       .then(resArrayOfData => {
-        let i = 0;
         return resArrayOfData.reduce((accDataPoints, currDataPoint) => {
+          let i = 0;
           const sensors = Object.keys(currDataPoint.data);
           while(i < sensors.length){
             const pointBySensor = {
               sensor: sensors[i],
               value : currDataPoint.data[sensors[i]].averageValue,
               STD: currDataPoint.data[sensors[i]].standardDeviation,
-              date: currDataPoint.piTimestamp
+              timeStamp: currDataPoint.piTimestamp
             };
             i++;
             accDataPoints.push(pointBySensor);
           }
-          return accDataPoints;
+          return accDataPoints.sort(function(a, b){
+            if(a.timeStamp < b.timeStamp) return -1;
+            if(a.timeStamp > b.timeStamp) return 1;
+            if(a.timeStamp === b.timeStamp) return 0;
+          });
         }, []);
       });
   };
+
 
   const dataListItems = dataPoints.map(...dataArrayObject => {
     (
       <>
         <li>{dataArrayObject.value}</li>
         <li>{dataArrayObject.STD}</li>
-        <li>{dataArrayObject.date}</li>
+        <li>{dataArrayObject.timeStamp}</li>
       </>
     );
   });
@@ -54,7 +59,7 @@ const Details = () => {
   );
 
 
-  //   const line = d3.line()
+  //   const line = d3.line();
   //   line
   //     .x(dataPoints.map(...dataArrayObject => dataArrayObject.date))
   //     .y(dataPoints.map(...dataArrayObject => dataArrayObject.value));
