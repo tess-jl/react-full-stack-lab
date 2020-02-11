4ef7b3f5597d0edb85667e2ce3e28d4ea635d31b
Ben Beekman <bbeekman@gmail.com>
2020-02-11 13:18:01 -0800

    quick fix

--- src/components/Auth/SignUp/SignUp.js
+++ src/components/Auth/SignUp/SignUp.js
@@ -7,10 +7,10 @@ const SignUp = () => {
   return (
     <main>
       <p>Sign Up</p>
-      <form onSubmit={ handleSubmit }>
-        <input type="text" name="email" value={ email } placeholder="Enter your email" onChange={ handleChange }></input>
-        <input type="text" name="password" value={ password } placeholder="Enter a password" onChange={ handleChange }></input>
-        <input type="text" name="piNickname" value={ piNickname } placeholder="Your Pi Nickname" onChange={ handleChange }></input>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="email" value={email} placeholder="Enter your email" onChange={handleChange}></input>
+        <input type="text" name="password" value={password} placeholder="Enter a password" onChange={handleChange}></input>
+        <input type="text" name="piNickname" value={piNickname} placeholder="Your Pi Nickname" onChange={handleChange}></input>
         <button>Create Account</button>
       </form>
     </main>
