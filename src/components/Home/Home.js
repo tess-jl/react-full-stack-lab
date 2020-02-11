8616337d74521e596340a7d09559ac9d91a40dc5
Ian Andrewson <ianandrewson@gmail.com>
2020-02-10 16:57:34 -0800

    removed head from home component since it lives in app

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import DataSessionDetails from './DataSessionDetails/DataSessionDetails.js';
-import Header from '../Header/Header.js';
 
 
 export default function Home() {
   return (
     <>
-      <Header />
       <p>This is the Home Page</p>
       <p>It needs Header, Footer Pi/Session List, and Session details</p>
       <DataSessionDetails />
