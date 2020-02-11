91d10959241389001a0d4864cb119c1c896d33dd
Tess Lameyer <tjlameyer@gmail.com>
2020-02-11 13:38:11 -0800

    request file drafted

---
+++ src/services/request.js
@@ -0,0 +1,20 @@
+const request = (path, method, body) => {
+  return fetch(`${process.env.API_URL}${path}`, {
+    method, 
+    headers: {
+      'Content-Type': 'application/json'
+    }, 
+    credentials: 'include', 
+    body: JSON.stringify(body)
+  })
+    .then(res => Promise.all([res.ok, res.jscn()]))
+    .then(([ok, json]) => {
+      if(!ok) throw json; 
+      return json;
+    });
+};
+
+export const get = path => request(path, 'GET');
+export const post = (path, body) => request(path, 'POST', body);
+export const patch = (path, body) => request(path, 'PATCH', body);
+export const del = path => request(path, 'DELETE');
